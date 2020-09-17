62ebee7dcca89778927cede94b678f705cd31f19
matias.lioneldamico@gmail.com <matias.lioneldamico@gmail.com>
2020-09-16 22:54:38 -0300

    public and private routes

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,25 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useForm } from '../hooks/useForm';
 import { startLogin } from '../store/actions/auth';
 import StyledForm from '../styles/StyledForm';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 interface LoginProps {}
 
 export const Login: React.FC<LoginProps> = ({}) => {
   const dispatch = useDispatch();
   const history = useHistory();
+  const { name } = useSelector((state) => state.auth);
   const [formValues, handleInputChange]: any = useForm({
     email: '',
     password: '',
   });
+  useEffect(() => {
+    if (name) {
+      history.push('/');
+    }
+    console.log(name);
+  }, [dispatch]);
   const onSubmitHandler = (e) => {
     e.preventDefault();
     dispatch(startLogin(email, password));
     history.push('/');
   };
 
-  const { name, email, password } = formValues;
+  const { email, password } = formValues;
   return (
     <StyledForm>
       <h2>Login</h2>
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -8,13 +8,15 @@ import { Navbar } from '../components/Navbar';
 import { Register } from '../pages/Register';
 import styled from 'styled-components';
 import { Login } from '../pages/Login';
+import { PublicRouter } from './PublicRouter';
+import { PrivateRouter } from './PrivateRouter';
+import { Cart } from '../pages/Cart';
 
 const StyledRouterContainer = styled.div`
   height: 100%;
 `;
-interface AppRouterProps {}
 
-export const AppRouter: React.FC<AppRouterProps> = ({}) => {
+export const AppRouter = ({}) => {
   const dispatch = useDispatch();
 
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -36,8 +38,21 @@ export const AppRouter: React.FC<AppRouterProps> = ({}) => {
         <StyledRouterContainer>
           <Switch>
             <Route exact component={ProductsPage} path='/' />
-            <Route exact component={Register} path='/register' />
-            <Route exact component={Login} path='/login' />
+            <PrivateRouter
+              isAuthenticated={isLoggedIn}
+              component={Cart}
+              path='/cart'
+            />
+            <PublicRouter
+              isAuthenticated={isLoggedIn}
+              component={Register}
+              path='/register'
+            />
+            <PublicRouter
+              isAuthenticated={isLoggedIn}
+              component={Login}
+              path='/login'
+            />
           </Switch>
         </StyledRouterContainer>
       </Router>
